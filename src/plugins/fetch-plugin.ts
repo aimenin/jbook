a826e83b84702898bd16e04db89a8f8257d47f8c
aimen <artem.igorevich8@rambler.ru>
2022-07-15 18:25:19 +0300

    refactor: split our onLoad to three onload for differents types of files

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -10,14 +10,14 @@ export const fetchPlugin = (inputCode: string) => {
   return {
     name: 'fetch-plugin',
     setup(build: esbuild.PluginBuild) {
-      build.onLoad({ filter: /.*/ }, async (args: any) => {
-        if (args.path === 'index.js') {
-          return {
-            loader: 'jsx',
-            contents: inputCode,
-          };
-        }
+      build.onLoad({ filter: /(^index\.js$)/ }, () => {
+        return {
+          loader: 'jsx',
+          contents: inputCode,
+        };
+      });
 
+      build.onLoad({ filter: /.css$/ }, async (args: any) => {
         /** check if we have lib in our cache */
         const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
           args.path
@@ -30,24 +30,43 @@ export const fetchPlugin = (inputCode: string) => {
         /** if we don't have lib in out cache we fetch it and put in our cache */
         const { data, request } = await axios.get(args.path);
 
-        const fileType = args.path.match(/.css$/) ? 'css' : 'jsx';
-
         const escaped = data
           .replace(/\n/g, '')
           .replace(/"/g, '\\"')
           .replace(/'/g, "\\'");
-        const contents =
-          fileType === 'css'
-            ? `
-            const style = documet.createElement('style');
-            style.innerText = '${escaped}';
-            document.head.appendChild(style);
-          `
-            : data;
+        const contents = `
+          const style = documet.createElement('style');
+          style.innerText = '${escaped}';
+          document.head.appendChild(style);
+        `;
 
         const result: esbuild.OnLoadResult = {
           loader: 'jsx',
           contents,
+          resolveDir: new URL('./', request.responseURL).pathname,
+        };
+
+        await fileCache.setItem(args.path, result);
+
+        return result;
+      });
+
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
+        /** check if we have lib in our cache */
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
+          args.path
+        );
+
+        if (cachedResult) {
+          return cachedResult;
+        }
+
+        /** if we don't have lib in out cache we fetch it and put in our cache */
+        const { data, request } = await axios.get(args.path);
+
+        const result: esbuild.OnLoadResult = {
+          loader: 'jsx',
+          contents: data,
           resolveDir: new URL('./', request.responseURL).pathname,
         };
 
