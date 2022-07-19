fd504252e3314208556f65de040470b6fe219094
aimen <artem.igorevich8@rambler.ru>
2022-07-19 16:23:15 +0300

    feat: add more settings to our editor

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -1,13 +1,26 @@
 import MonacoEditor from '@monaco-editor/react';
+import { FC } from 'react';
 
-const CodeEditor = () => {
+interface CodeEditorProps {
+  initialValue: string;
+}
+
+const CodeEditor: FC<CodeEditorProps> = ({ initialValue }) => {
   return (
     <MonacoEditor
       theme="dark"
       language="javascript"
       height="500px"
+      value={initialValue}
       options={{
         wordWrap: 'on',
+        minimap: { enabled: false },
+        showUnused: false,
+        folding: false,
+        lineNumbersMinChars: 3,
+        fontSize: 16,
+        scrollBeyondLastLine: false,
+        automaticLayout: true,
       }}
     />
   );
