23cd1b7104af40559e125fd6bd6e7b375cd99c6d
aimen <artem.igorevich8@rambler.ru>
2022-07-19 16:59:14 +0300

    feat: add prettier to code editor

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useRef } from 'react';
 import MonacoEditor, { EditorDidMount } from '@monaco-editor/react';
 import prettier from 'prettier';
 import parser from 'prettier/parser-babel';
@@ -9,7 +9,10 @@ interface CodeEditorProps {
 }
 
 const CodeEditor: FC<CodeEditorProps> = ({ initialValue, onChange }) => {
+  const editorRef = useRef<any>();
+
   const onEditorDidMount: EditorDidMount = (getValue, monacoEditor) => {
+    editorRef.current = monacoEditor;
     monacoEditor.onDidChangeModelContent(() => {
       onChange(getValue());
     });
@@ -17,7 +20,17 @@ const CodeEditor: FC<CodeEditorProps> = ({ initialValue, onChange }) => {
     monacoEditor.getModel()?.updateOptions({ tabSize: 2 });
   };
 
-  const onFormatClick = () => {};
+  const onFormatClick = () => {
+    const unformatted = editorRef.current.getModel().getValue();
+    const formatted = prettier.format(unformatted, {
+      parser: 'babel',
+      plugins: [parser],
+      useTabs: false,
+      semi: true,
+      singleQuote: true,
+    });
+    editorRef.current.setValue(formatted);
+  };
 
   return (
     <div>
