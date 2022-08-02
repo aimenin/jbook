8e7aa45888ffb8bfbe2fadbbbb949348197278ed
aimen <artem.igorevich8@rambler.ru>
2022-08-02 16:54:24 +0300

    feat: add initial state for our application

--- src/state/reducers/index.ts
+++ src/state/reducers/index.ts
@@ -1,1 +1,11 @@
-import React from 'react';
+import { combineReducers } from 'redux';
+
+import cellsReducer from './cellsReducer';
+
+const reducers = combineReducers({
+  cells: cellsReducer,
+});
+
+export default reducers;
+
+export type RootState = ReturnType<typeof reducers>;
