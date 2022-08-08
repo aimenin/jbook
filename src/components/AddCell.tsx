885624e91e77c6ecdc961fb764b3148bc3fc5212
aimen <artem.igorevich8@rambler.ru>
2022-08-08 23:57:45 +0300

    feat: add addCell component and bind it with redux

---
+++ src/components/AddCell.tsx
@@ -0,0 +1,21 @@
+import { FC } from 'react';
+
+import { useActions } from '../hooks/use-actions';
+import './AddCell.css';
+
+interface AddCellProps {
+  nextCellId: string;
+}
+
+const AddCell: FC<AddCellProps> = ({ nextCellId }) => {
+  const { insertCellBefore } = useActions();
+
+  return (
+    <div>
+      <button onClick={() => insertCellBefore(nextCellId, 'code')}>Code</button>
+      <button onClick={() => insertCellBefore(nextCellId, 'text')}>Text</button>
+    </div>
+  );
+};
+
+export default AddCell;
