f7a7bc5d3b3f61597e3f47f5a4a2094e2cfea524
aimen <artem.igorevich8@rambler.ru>
2022-08-08 13:25:27 +0300

    feat: add cell item component logic

--- src/components/CellList.tsx
+++ src/components/CellList.tsx
@@ -1,7 +1,24 @@
-import { FC } from "react";
+import { FC } from 'react';
+
+import { useTypedSelector } from '../hooks/use-typed-selector';
+import CellListItem from './CellListItem';
 
 const CellList: FC = () => {
-  return <div>CellList</div>
-}
+  const cells = useTypedSelector((state) => {
+    const { cells } = state;
+    if (cells) {
+      const { order, data } = cells;
+      return order.map((id) => {
+        return data[id];
+      });
+    }
+  });
+
+  const renderedCells = cells!.map((cell) => (
+    <CellListItem key={cell.id} cell={cell} />
+  ));
+
+  return <div>{renderedCells}</div>;
+};
 
 export default CellList;
