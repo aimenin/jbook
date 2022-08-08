ba4c5edd65aa8a4dee75eb4b3e98dcf8cfc6eaa8
aimen <artem.igorevich8@rambler.ru>
2022-08-08 13:06:54 +0300

    feat: add sceleton of two new components

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,13 @@ import { Provider } from 'react-redux';
 
 import { store } from './state';
 import TextEditor from './components/TextEditor';
+import CellList from './components/CellList';
 
 const App = () => {
   return (
     <Provider store={store}>
       <div>
-        <TextEditor />
+        <CellList />
       </div>
     </Provider>
   );
