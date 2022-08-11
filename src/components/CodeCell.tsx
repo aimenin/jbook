137adcd1ac7f5967f2fe4dcca1355001c5b38187
aimen <artem.igorevich8@rambler.ru>
2022-08-11 17:22:42 +0300

    feat: add styles to progress bar

--- src/components/CodeCell.tsx
+++ src/components/CodeCell.tsx
@@ -7,6 +7,8 @@ import { Cell } from '../state';
 import { useActions } from '../hooks/use-actions';
 import { useTypedSelector } from '../hooks/use-typed-selector';
 
+import './CodeCell.css';
+
 interface CodeCellProps {
   cell: Cell;
 }
@@ -46,7 +48,15 @@ const CodeCell: FC<CodeCellProps> = ({ cell }) => {
             onChange={(value) => updateCell(cell.id, value)}
           />
         </Resizable>
-        {bundle && <Preview code={bundle.code} err={bundle.err} />}
+        {!bundle || bundle.loading ? (
+          <div className="progress-cover">
+            <progress className="progress is-small is-primary" max="100">
+              Loading
+            </progress>
+          </div>
+        ) : (
+          <Preview code={bundle.code} err={bundle.err} />
+        )}
       </div>
     </Resizable>
   );
