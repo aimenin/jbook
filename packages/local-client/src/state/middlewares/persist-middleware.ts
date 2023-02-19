badfbec3dbd73dee807b4c9d4087f06dc225e82e
aimen <artem.igorevich8@rambler.ru>
2023-02-19 20:46:18 +0300

    add functionality to pesist middleware

--- packages/local-client/src/state/middlewares/persist-middleware.ts
+++ packages/local-client/src/state/middlewares/persist-middleware.ts
@@ -1,11 +1,29 @@
-import { Dispatch } from 'react';
+import { Dispatch } from 'redux';
 import { Action } from '../actions';
 import { ActionType } from '../action-types';
 import { saveCells } from '../action-creators';
+import { RootState } from '../reducers';
 
 export const persistMiddleware =
-  ({ dispatch }: { dispatch: Dispatch<Action> }) =>
+  ({
+    dispatch,
+    getState,
+  }: {
+    dispatch: Dispatch<Action>;
+    getState: () => RootState;
+  }) =>
   (next: (action: Action) => void) =>
   (action: Action) => {
     next(action);
+
+    if (
+      [
+        ActionType.MOVE_CELL,
+        ActionType.UPDATE_CELL,
+        ActionType.INSERT_CELL_AFTER,
+        ActionType.DELETE_CELL,
+      ].includes(action.type)
+    ) {
+      saveCells()(dispatch, getState);
+    }
   };
